32c82bee0a1e02cbaff9a68ad5a8a8c8767c673c
DandinPower <tomhot246@gmail.com>
2024-02-25 12:31:42 +0800

    add: ensure dir exist

---
+++ dumper/utils.py
@@ -0,0 +1,8 @@
+import os 
+
+def ensure_dir_exists_by_filename(filename: str) -> None:
+    """Ensure that the directory of a file exists."""
+    dirname = os.path.dirname(filename)
+    print(dirname)
+    if dirname:
+        os.makedirs(dirname, exist_ok=True)
